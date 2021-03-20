42d9d1f658a9c15fd7ca91a655fc2ce6fee3ae7b
Green S <kaylasierrabartley@gmail.com>
2021-03-19 19:00:40 -0500

    added madols and associations

--- models/index.js
+++ models/index.js
@@ -5,13 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+//which tags a single product has
+Product.belongsToMany(Tag, {
+  through: ProductTag, 
+  as: 'tagged_products',
+  foreignKey: product_id
+})
 // Tags belongToMany Products (through ProductTag)
-
+//which products have a particular tag
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  as: 'tagged_products',
+  foreignKey: tag_id
+})
 module.exports = {
   Product,
   Category,
